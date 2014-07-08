b40a315e8924c8a145f71b2866f2c44c5e7ec4cd
Jason Carney <jason@dashmedia.com.au>
2014-07-08 15:17:20 +0930

    enable development mode on global cache clear
    Signed-off-by: Jason Carney <jason@dashmedia.com.au>

--- PurgeCache.php
+++ PurgeCache.php
@@ -7,12 +7,20 @@ foreach ($contexts as $context) {
     $token =  $contextObj->getOption('cf_api_key');
     $email =  $contextObj->getOption('cf_email');
     $skip =  $contextObj->getOption('cf_skip') || 0;
+    $devMode =  $contextObj->getOption('cf_use_dev') || 1;
     $data = array(
         "a" => "fpurge_ts", //action
         "tkn" => $token, //account token
         "email" => $email, //email address associated with account
         "z" => $domain, //Target Domain
         "v" => "1" //just set it to 1
+        );
+    $devData = array(
+        "a" => "devmode", //action
+        "tkn" => $token, //account token
+        "email" => $email, //email address associated with account
+        "z" => $domain, //Target Domain
+        "v" => "1" //enable
         );
     if($skip != 1 && $domain != '' && !is_null($domain)){
         $ch = curl_init("https://www.cloudflare.com/api_json.html");
@@ -27,5 +35,19 @@ foreach ($contexts as $context) {
         }
         ob_flush();
         curl_close($ch);
+        if($devMode){
+            $ch = curl_init("https://www.cloudflare.com/api_json.html");
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $devData);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $result = json_decode(curl_exec($ch));
+            if($result->result == 'success'){
+                $modx->log(MODX_LOG_LEVEL_INFO,'Cloudflare ('.$domain.'): Development mode activated');
+            } else {
+                $modx->log(MODX_LOG_LEVEL_ERROR,'Cloudflare ('.$domain.'):' . $result->msg);
+            }
+            ob_flush();
+            curl_close($ch);
+        }
     }
 }
--- README.md
+++ README.md
@@ -13,8 +13,14 @@ Goto System Settings and update the following:
 
 Please note that PurgeCloudFlare relies on your `http_host` setting to tell CloudFlare which domain/account to clear files from, if you're using multiple contexts they will each need this setting.
 
-Ignoring a context: if you do not wish PurgeCloudFlare to attempt to clear the CloudFlare cache for a specific context, add a context setting named `cf_skip` and set its value to `1`
-
 Once you have done this, MODX will clear your CloudFlare Cache every time the system-wide cache is cleared, and will clear individual pages when they are saved from the MODX manager
+
+##Ignoring a context
+
+If you do not wish PurgeCloudFlare to attempt to clear the CloudFlare cache for a specific context, add a context setting named `cf_skip` and set its value to `1`
+
+##Development mode
+
+MODX will automatically enable development mode when clearing the global cache, this behaviour can be prevented by creating a context setting of `cf_use_dev` and setting it to 0 on any contexts you wish.
 
 Created by Jason Carney - DashMedia.com.au

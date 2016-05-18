eddabc63fdc55895a0c428d9fae47b9a21b3680d
friimaind <massimiliano.monaro@gmail.com>
2016-05-18 18:40:58 +0200

    first release of PurgeCache V4

--- elements/plugins/PurgeCache.php
+++ elements/plugins/PurgeCache.php
@@ -1,69 +1,83 @@
 <?php
+
 /**
  * @name PurgeCache
  * @description This is used to clear your CloudFlare cache when the clear cache button is pressed in the main site menu
  * @PluginEvents OnBeforeCacheUpdate
+ * @author Massimiliano Monaro <massimiliano.monaro@gmail.com>
  */
 
-$core_path = $modx->getOption('cloudflare.core_path', null, MODX_CORE_PATH.'components/cloudflare/');
-include_once $core_path .'vendor/autoload.php';
+/*
+ * API Credentials
+ */
+
+$email = $modx->getOption('cloudflare.email_address');
+$token = $modx->getOption('cloudflare.api_key');
 
 $contexts = $modx->getCollection('modContext', array('key:NOT IN' => array('mgr')));
 foreach ($contexts as $context) {
-    // do stuff with each modContext object
+
+    /*
+     * Get Zone ID of the context
+     */
+
     $contextObj = $modx->getContext($context->key);
-    $domain = $contextObj->getOption('http_host');
-    $token =  $contextObj->getOption('cloudflare.api_key');
-    $email =  $contextObj->getOption('cloudflare.email_address');
-    $skip =  $contextObj->getOption('cloudflare.skip') || 0;
-    $devMode =  intval($contextObj->getOption('cloudflare.use_dev'));
+    $skip = $contextObj->getOption('cf_skip') || 0;
+    $http_host = str_replace("www.", "", $contextObj->getOption('http_host'));
+    $dev_mode = intval($contextObj->getOption('cloudflare.use_dev'));
 
-    //check if host starts with 'www'
-    if(preg_match('/^www\.(.+)$/',$domain, $matches)){
-        //remove the www
-        $domain = $matches[1];
-    }
+    $headers = [
+        'X-Auth-Email: ' . $email,
+        'X-Auth-Key: ' . $token,
+        'Content-Type: application/json'
+    ];
 
-    $data = array(
-        "a" => "fpurge_ts", //action
-        "tkn" => $token, //account token
-        "email" => $email, //email address associated with account
-        "z" => $domain, //Target Domain
-        "v" => "1" //just set it to 1
-        );
-    $devData = array(
-        "a" => "devmode", //action
-        "tkn" => $token, //account token
-        "email" => $email, //email address associated with account
-        "z" => $domain, //Target Domain
-        "v" => "1" //enable
-        );
-    if($skip != 1 && $domain != '' && !is_null($domain)){
-        $ch = curl_init("https://www.cloudflare.com/api_json.html");
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = json_decode(curl_exec($ch));
-        if($result->result == 'success'){
-            $modx->log(MODX_LOG_LEVEL_INFO,'Cloudflare: cache for ' . $domain . ' successfully cleared');
+    $ch = curl_init('https://api.cloudflare.com/client/v4/zones?name=' . $http_host . '&status=active&page=1&per_page=20&order=status&direction=desc&match=all');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    $result = json_decode(curl_exec($ch), true);
+    curl_close($ch);
+
+    if ($skip != 1 && $result['success'] == 1) {
+        $zone_id = $result['result'][0]['id'];
+
+        // purge all cache        
+        $data = array("purge_everything" => true);
+        $ch = curl_init('https://api.cloudflare.com/client/v4/zones/' . $zone_id . '/purge_cache');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        $result = json_decode(curl_exec($ch), true);
+
+        if ($result['success'] == 1) {
+            $modx->log(MODX_LOG_LEVEL_INFO, 'CloudFlare: cache for ' . $http_host . ' successfully cleared');
         } else {
-            $modx->log(MODX_LOG_LEVEL_ERROR,'Cloudflare ('.$domain.'):' . $result->msg);
+            $modx->log(MODX_LOG_LEVEL_ERROR, 'CloudFlare (' . $http_host . '):' . $result['errors']);
         }
-        ob_flush();
+
         curl_close($ch);
-        if($devMode == 1){
-            $ch = curl_init("https://www.cloudflare.com/api_json.html");
-            curl_setopt($ch, CURLOPT_POST, true);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $devData);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $result = json_decode(curl_exec($ch));
-            if($result->result == 'success'){
-                $modx->log(MODX_LOG_LEVEL_INFO,'Cloudflare ('.$domain.'): Development mode activated');
+        ob_flush();
+
+        // set dev mode to on        
+        if ($dev_mode == 1) {
+            $data = array("value" => 'on');
+            $ch = curl_init('https://api.cloudflare.com/client/v4/zones/' . $zone_id . '/settings/development_mode');
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PATCH");
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+            $result = json_decode(curl_exec($ch), true);
+
+            if ($result['success'] == 1) {
+                $modx->log(MODX_LOG_LEVEL_INFO, 'CloudFlare (' . $http_host . '): Development mode activated');
             } else {
-                $modx->log(MODX_LOG_LEVEL_ERROR,'Cloudflare ('.$domain.'):' . $result->msg);
+                $modx->log(MODX_LOG_LEVEL_ERROR, 'CloudFlare (' . $http_host . '):' . $result['errors']);
             }
-            ob_flush();
+
             curl_close($ch);
+            ob_flush();
         }
     }
 }
+
